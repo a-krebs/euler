4ad3523950079dcef0f1bf0d808a20247f6c535c
Aaron Krebs <akrebs@ualberta.ca>
2013-02-20 20:07:23 -0700

    Cleanup and comments.

--- 5/solution.c
+++ 5/solution.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
-#include "solution.h"
+
+/*
+ * Project Euler problem 5:
+ * 
+ * 2520 is the smallest number that can be divided by each of the numbers from
+ * 1 to 10 without any remainder.
+ *
+ * What is the smallest positive number that is evenly divisible
+ * by all of the numbers from 1 to 20?
+*/
 
 #define TRUE 1
 #define FALSE 0
+#define LOWER 1
+#define UPPER 20
 
 int main ( int argc, char *argv[])
 {
@@ -12,8 +23,10 @@ int main ( int argc, char *argv[])
 
 	while (TRUE)
 	{
-		candidate = i * 20;
-		for (int j = 1; j < 21; j++)
+		// candidates must be multiples of 20
+		candidate = i * UPPER;
+
+		for (int j = 1; j < UPPER + 1; j++)
 		{
 			if (candidate % j != 0)
 			{
@@ -22,11 +35,13 @@ int main ( int argc, char *argv[])
 		}
 		if (is_divisible == FALSE)
 		{
+			// try next candidate
 			i++;
 			is_divisible = TRUE;
 		} 
 		else 
 		{
+			// we've found the number
 			break;
 		}
 	}
