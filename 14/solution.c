dc5596b45bd834e51fd36d2e9edc878bc919dc0e
Aaron Krebs <akrebs@ualberta.ca>
2013-05-08 16:38:15 -0600

    Implemented cache for problem 14.

--- 14/solution.c
+++ 14/solution.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <assert.h>
 
 /*
  * Project Euler problem 14:
@@ -15,15 +16,26 @@
 
 #define MAX_N 999999
 
-int getLength(long n, int length)
-{
+int getLength(long n, int* cache)
+{	
+	int cached = 0;
 	if (n == 1) {
-		return length + 1;
-	} else if (n % 2 == 0) {
-		return getLength(n / 2, length + 1);
-	} else {
-		return getLength(3*n + 1, length + 1);
+		return 1;
 	}
+	if (n <= MAX_N) {
+		cached = cache[n];
+	}
+	if (cached != 0) {
+		return cached;
+	} else if (n % 2 == 0) {
+		cached = getLength(n / 2, cache) + 1;
+	} else {
+		cached = getLength(3*n + 1, cache) + 1;
+	}
+	if (n <= MAX_N) {
+		cache[n] = cached;
+	}
+	return cached;
 }
 
 int main(int argc, char *argv[])
@@ -31,12 +43,15 @@ int main(int argc, char *argv[])
 	long n_longest_chain = 0;
 	int longest_chain_length = 0;
 	long tmp = 0;
+	int* cache = calloc(MAX_N + 1, sizeof(int));
 	for (long n = MAX_N; n > 0; n--) {
-		tmp = getLength(n, 0);
+		tmp = getLength(n, cache);
 		if (longest_chain_length < tmp) {
 			n_longest_chain = n;
 			longest_chain_length = tmp;
 		}
 	}
 	printf("%ld\n", n_longest_chain);
+	free(cache);
+	exit(EXIT_SUCCESS);
 }
